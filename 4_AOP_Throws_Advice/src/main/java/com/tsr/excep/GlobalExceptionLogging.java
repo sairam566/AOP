45ef19a4e7569b5c10352338fb26f6278fa83e80
sairam566 <sairam1431995@gmail.com>
2023-05-01 10:52:13 +0530

    Secondary Logic

---
+++ 4_AOP_Throws_Advice/src/main/java/com/tsr/excep/GlobalExceptionLogging.java
@@ -0,0 +1,13 @@
+package com.tsr.excep;
+
+import java.lang.reflect.Method;
+
+import org.springframework.aop.ThrowsAdvice;
+
+public class GlobalExceptionLogging implements ThrowsAdvice{
+	
+	public void afterThrowing(Method method, Object[] args, Object target, IllegalArgumentException ex) {
+		System.out.println("Method Name: "+method.getName());
+		System.out.println("Throws the following Exception: "+ex);
+	}
+}
