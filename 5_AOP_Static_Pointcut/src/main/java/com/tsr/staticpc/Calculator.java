89bdfcb5498d181f5f76f98c2f12cce107668aa9
sairam566 <sairam1431995@gmail.com>
2023-05-04 11:13:35 +0530

    Primary business logic

---
+++ 5_AOP_Static_Pointcut/src/main/java/com/tsr/staticpc/Calculator.java
@@ -0,0 +1,17 @@
+package com.tsr.staticpc;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+
+	public int substract(int a, int b) {
+		return b - a;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+}
