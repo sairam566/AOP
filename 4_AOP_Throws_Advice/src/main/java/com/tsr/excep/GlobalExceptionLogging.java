9263506d262dab9b423964d1d584a46c60b98d9d
sairam566 <sairam1431995@gmail.com>
2023-05-04 10:13:33 +0530

    Added second Exception

--- 4_AOP_Throws_Advice/src/main/java/com/tsr/excep/GlobalExceptionLogging.java
+++ 4_AOP_Throws_Advice/src/main/java/com/tsr/excep/GlobalExceptionLogging.java
@@ -6,6 +6,10 @@ import org.springframework.aop.ThrowsAdvice;
 
 public class GlobalExceptionLogging implements ThrowsAdvice{
 	
+	public void afterThrowing(IllegalArgumentException ex) {
+		System.out.println("2: "+ex);
+	}
+	
 	public void afterThrowing(Method method, Object[] args, Object target, IllegalArgumentException ex) {
 		System.out.println("Method Name: "+method.getName());
 		System.out.println("Throws the following Exception: "+ex);
