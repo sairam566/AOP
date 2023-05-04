ef95bd06f019ab8b1d89075b853d91045d441bce
sairam566 <sairam1431995@gmail.com>
2023-05-04 11:13:43 +0530

    Primary business logic

---
+++ 6_AOP_Dynamic_Pointcut/src/main/java/com/tsr/dynamicpc/Calculator.java
@@ -0,0 +1,17 @@
+package com.tsr.dynamicpc;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+
+	public int substract(int a, int b) {
+		return b - a;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+}
