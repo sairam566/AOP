1699017f49ed846e5fad1a46dbd576af3c89d8d6
sairam566 <sairam1431995@gmail.com>
2023-05-08 12:02:39 +0530

    Primary business logic

---
+++ 7_Declarative_Aspectj_Around/src/main/java/com/tsr/beans/Calculator.java
@@ -0,0 +1,20 @@
+package com.tsr.beans;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int add(int a, int b, int c) {
+		return a + b + c;
+	}
+
+	public int substract(int a, int b) {
+		return b - a;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+}
