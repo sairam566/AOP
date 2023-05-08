7ab5d477bfe65a77449517cd08b174bf420a221d
sairam566 <sairam1431995@gmail.com>
2023-05-08 12:04:08 +0530

    Declarative Aspectj (Around Aspect)

---
+++ 7_Declarative_Aspectj_Around/src/main/java/com/tsr/App.java
@@ -0,0 +1,23 @@
+package com.tsr;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.tsr.beans.Calculator;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/tsr/conf/spring-context.xml");
+
+		Calculator calculator = applicationContext.getBean("cal", Calculator.class);
+
+		int sum1 = calculator.add(10, 20);
+		System.out.println("Sum : " + sum1);
+		
+		int sum2 = calculator.add(10, 20, 30);
+		System.out.println("Sum : " + sum2);
+		
+		int sub = calculator.substract(10, 20);
+		System.out.println("Substraction : " + sub);
+	}
+}
